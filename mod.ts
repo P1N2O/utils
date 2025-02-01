b70a1a94ddc4a3c30954bd7ebd584938542eda22
Manuel Pinto <manuel@pinto.dev>
2025-02-01 11:25:19 +0000

    chore(mod): remove main module example

--- mod.ts
+++ mod.ts
@@ -1,17 +1,4 @@
 // Copyright (c) 2025 Manuel Pinto
 // This code is licensed under MIT license (see LICENSE for details)
 
-/**
- * A collection of utility functions for common JavaScript/TypeScript tasks
- *
- * ```ts ignore
- * import { parseUserAgent } from "@p1n2o/utils/user-agent";
- *
- * const ua = parseUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
- * console.log(ua)
- * ```
- *
- * @module
- */
-
 // export * from "./lib/user-agent/main.ts";
